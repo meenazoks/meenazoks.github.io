a059a6befe60ed4f6c15aa2089dece983c8dfdd8
Meena Zoks <99848498+meenazoks@users.noreply.github.com>
2022-12-02 18:19:42 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+
+/* Allows collapsable project description to open and 
+close when clicked on */
+
+var coll = document.getElementsByClassName("learn-more");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+    });
+}
